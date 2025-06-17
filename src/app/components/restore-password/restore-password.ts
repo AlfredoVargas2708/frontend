3102d8e62f703786a969a8f7b09ffdc4b0eead99
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-06-16 20:10:07 -0400

    implement restore password functionality with form handling and service integration

--- src/app/components/restore-password/restore-password.ts
+++ src/app/components/restore-password/restore-password.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-restore-password',
@@ -11,11 +12,26 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class RestorePassword {
   passwordForm: FormGroup
   
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private usersService: UsersService) {
     this.passwordForm = this.fb.group({
       email: [''],
       password: [''],
       confirmPassword: ['']
     });
+  }
+
+  resetPassword() {
+    if (this.passwordForm.valid) {
+      this.usersService.resetPassword(this.passwordForm.value).subscribe({
+        next: (res) => {
+          console.log('Password reset successful', res);
+        },
+        error: (error) => {
+          console.error('Error resetting password', error);
+        }
+      });
+    } else {
+      console.error('Form is invalid');
+    }
   }
 }
